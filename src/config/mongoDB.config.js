89f2f3854c4be359ae14afec1fa36dff8a4ec303
Solange Fano <57511747+s0lange86@users.noreply.github.com>
2024-06-22 16:37:39 -0300

    configuracion mongo

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //funcion para determinar la conexion de mongo atlas:
 export const connectMongoDB = async () => {
     try {
-        mongoose.connect("mongodb+srv://admin:123@coder.zu2v8jq.mongodb.net/proyectoBackend");
+        mongoose.connect("");
         console.log("MongoDB connected");
     } catch (error) {
         console.log(`Error: ${error}}`);
